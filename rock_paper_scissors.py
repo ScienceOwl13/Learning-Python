a6f8d46d62250dc77ca957b97c635bb5c59684c7
ScienceOwl13 <groencg@gmail.com>
2022-02-23 20:14:41 -0500

    Wrong answer for tiebreaking question fix

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,7 +6,7 @@ import random
 from time import sleep
 
 
-print("Welcome to Connor's Rock, Paper, Scissors Game.")
+tieGame = input("Welcome to Connor's Rock, Paper, Scissors Game. ")
 numOfRounds = int(input("How many games would you like to play? "))
 
 roundsPlayed = 0
@@ -99,13 +99,96 @@ def playRockPaperScissors():
         playRockPaperScissors()
 
 def TieBreakingRound():
+
+    global computerScore
+    global playerScore
+
     input(f"\nThe scores are tied at {computerScore} for the computer and {playerScore} for you! Are you ready for the tie breaking round? ")
 
+        #Chooses a number between 1 and 3. 1 is rock, 2 is paper, 3 is scissors.
+    computerMove = random.randint(1,3)
+
+    #Asks player to for their move
+    playerMove = input("Do you choose rock, paper or scissors? ")
+
+    #Determins who wins.
+
+    #Computer Chooses Rock
+        #Rock vs Rock
+    if computerMove == 1 and playerMove == "rock":
+        print("You chose: 'rock' and the computer chose 'rock'. No one gets a point.")
+        print("Redoing this match.")
+        playRockPaperScissors()
+
+        #Rock vs Paper
+    elif computerMove == 1 and playerMove == "paper":
+        print("You chose: 'paper' and the computer chose: 'rock'. You win!")
+        playerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+        #Rock vs Scissors
+    elif computerMove == 1 and playerMove == "scissors":
+        print("You chose: 'scissors' and the computer chose: 'rock'. The computer wins!")
+        computerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+    #Computer Chooses Paper
+        #Paper vs Rock
+    elif computerMove == 2 and playerMove == "rock":
+        print("You chose: 'rock' and the computer chose: 'paper'. The computer wins!")
+        computerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+        #Paper vs Paper
+    elif computerMove == 2 and playerMove == "paper":
+        print("You chose: 'paper' and the computer chose: 'paper'. No one gets a point.")
+        print("Redoing this match.")
+        playRockPaperScissors()
+
+        #Paper vs Scissors
+    elif computerMove == 2 and playerMove == "scissors":
+        print("You chose: 'scissors' and the computer chose: 'paper'. You win!")
+        playerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+    #Computer Chooses Scissors
+        #Scissors vs Rock
+    elif computerMove == 3 and playerMove == "rock":
+        print("You chose: 'rock' and the computer chose: 'scissors'. You win!")
+        playerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+        #Scissors vs Paper
+    elif computerMove == 3 and playerMove == "paper":
+        print("You chose: 'paper' and the computer chose: 'Scissors'. The computer wins!")
+        computerScore += 1
+        print(f"You have {playerScore} and the computer has {computerScore}")
+
+        #Scissors vs Scissors
+    elif computerMove == 3 and playerMove == "scissors":
+        print("You chose: 'scissors' and the computer chose: 'scissors'. No one gets a point.")
+        print("Redoing this match.")
+        playRockPaperScissors()
+
+        #If wrong option is chosen and it's the tie-breaking round
+    elif playerMove != "rock" or "paper" or "scissors" and isTieBreakingRound == True:
+        print("\nYou have typed in something that is not an option. You get to try again:")
+        TieBreakingRound()
+
+        #If wrong option is chosen
+    else:
+        print(f"You chose: {playerMove}, which is not valid. Please choose either 'rock', 'paper' or 'scissors'.")
+        print("Here is a redo of this round:")
+        playRockPaperScissors()
+
+
+#Stuff that needs to be in here for the game
 
 if numOfRounds == 0:
     print(f"You have selected to play 0 rounds. Quitting the game.")
     sleep(3)
     quit()
+
 
 while numOfRounds != roundsPlayed:
 
@@ -115,6 +198,7 @@ while numOfRounds != roundsPlayed:
 if playerScore == computerScore:
     isTieBreakingRound = True
     TieBreakingRound()
+    isTieBreakingRound = False
     
 
 if playerScore < computerScore:
@@ -124,6 +208,12 @@ else:
 
 print("\nThank you for playing Rock, Paper, Scissors.")
 
-sleep(10)
+#Secret developer settings:
+
+#At the very first prompt, put "*" to make the round a tie breaking round. You need to answer "1" for the amount of rounds, then get the first prompt wrong.
+if tieGame == "*":
+    isTieBreakingRound = True
+
+sleep(5)
 
 exit()
